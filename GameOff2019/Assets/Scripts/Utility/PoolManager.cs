d2bc2234d83c023d60f2804413c63b604aa7d37c
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2019-11-01 18:38:55 -0400

    Initial Project Push
    Put together a shell of the Unity project using v 2019.2.10f1.
    Basically empty at this point with the exception of a few utility scripts and folders

---
+++ GameOff2019/Assets/Scripts/Utility/PoolManager.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class creates and manages object pools.  Improvements to make still:  Handle multiple scenes, make class static (pros/cons vs singleton?)
+/// </summary>
+public class PoolManager : MonoBehaviour
+{
+    private Dictionary<int, List<GameObject>> poolDictionary = new Dictionary<int, List<GameObject>>();
+    private int dictionaryIndex = 0;
+    private GameObject[] prefabsToLoad;
+    static PoolManager _instance;
+
+    public bool IsInitialized = false;
+    public const string PooledObjectResourcesFolderFilePath = "";
+    public static PoolManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<PoolManager>();
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// Initializes all prefabs in the Resources folder.  Can use sub folder of resources by passing parameter
+    /// </summary>
+    public void Initialize(string filePath = PooledObjectResourcesFolderFilePath)
+    {
+        prefabsToLoad = Resources.LoadAll<GameObject>(PooledObjectResourcesFolderFilePath);
+
+        foreach (GameObject go in prefabsToLoad)
+        {
+            instance.CreatePool(go);
+        }
+
+        IsInitialized = true;
+    }
+
+    /// <summary>
+    /// Creates pool object given a prefab.  note that the prefab must be a Pool Object 
+    /// (or child of PoolObject class)
+    /// </summary>
+    /// <param name="prefab">Prefab to be pooled</param>
+    /// <param name="poolSize"></param>
+    public void CreatePool(GameObject prefab)
+    {
+        PooledObject poolObject = prefab.GetComponent<PooledObject>();
+        GameObject poolHolder = new GameObject(prefab.name + " pool");
+
+        // Probably need more error checking in this
+
+        if (poolObject == null)
+        {
+            Debug.Log("Pool NOT created.  Check if PoolObject script is attached to prefab: " + prefab.name);
+            return;
+        }
+        else
+        {
+            // Assigns the prefab an identity in the pool dictionary
+            // -1 is the default PoolID vaule for pooled objects
+            poolObject.PoolID = dictionaryIndex;
+
+            poolDictionary.Add(poolObject.PoolID, new List<GameObject>());
+
+            for (int i = 0; i < poolObject.StartingPoolSize; i++)
+            {
+                GameObject newObject = Instantiate(prefab) as GameObject;
+                newObject.SetActive(false);
+                newObject.GetComponent<PooledObject>().SetParent(poolHolder.transform);
+                poolDictionary[poolObject.PoolID].Add(newObject);
+            }
+
+            // Increment dictionaryIndex for next pool
+            dictionaryIndex++;
+        }
+       
+
+    }
+
+
+    /// <summary>
+    /// Returns a game object instance from its prefab pool if there are available instances 
+    /// in that pool.  Expands the pool if necessary.
+    /// </summary>
+    /// <param name="prefab">Pooled object prefab of which an instance will be returned</param>
+    /// <returns></returns>
+    public GameObject GetPooledObject(GameObject prefab)
+    {
+        int poolKey = prefab.GetComponent<PooledObject>().PoolID;
+
+        if (poolDictionary.ContainsKey(poolKey) && poolDictionary[poolKey].Count > 0)
+        {
+            GameObject poolItem = poolDictionary[poolKey][poolDictionary[poolKey].Count - 1];
+            poolDictionary[poolKey].RemoveAt(poolDictionary[poolKey].Count - 1);
+
+            return poolItem;
+        }
+        else 
+        {
+            ExpandPool(prefab, poolKey, prefab.GetComponent<PooledObject>().PoolExpansionFactor);
+            return GetPooledObject(prefab);
+        }
+
+    }
+
+    public void ReturnToPool(GameObject prefab)
+    {
+        int poolKey = prefab.GetComponent<PooledObject>().PoolID;
+
+        if (poolDictionary.ContainsKey(poolKey))
+        {
+            prefab.SetActive(false);
+            poolDictionary[poolKey].Add(prefab);
+
+        }
+
+    }
+
+    // NEED TO FIX THIS:  The expanded objects dont go into the parent object for organization
+    private void ExpandPool(GameObject prefab, int poolKey, int listExpansionFactor)
+    {
+        for (int i = 0; i < listExpansionFactor; i++)
+        {
+            GameObject newObject = Instantiate(prefab) as GameObject;
+            newObject.SetActive(false);
+            poolDictionary[poolKey].Add(newObject);
+        }
+
+    }
+
+    public void EmptyAllPools()
+    {
+        dictionaryIndex = 0;
+        poolDictionary.Clear();
+    }
+
+}
---
+++ GameOff2019/Assets/Scripts/Utility/PooledObject.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+/// <summary>
+/// Class that governs the behavior and parameters of an object to be pooled
+/// </summary>
+[RequireComponent(typeof(Transform))]
+public class PooledObject : MonoBehaviour
+{
+    public int StartingPoolSize = 10;
+    public int PoolExpansionFactor = 5;
+    public int PoolID = -1;
+
+
+    private void OnBecameInvisible()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            ReturnToPool();
+        }
+    }
+
+    /// <summary>
+    /// Returns object instance back to its pool using the pool manager and stops the object from moving
+    /// if it has a rigidbody. The pool manager will deactivate the object
+    /// </summary>
+    public virtual void ReturnToPool()
+    {
+        PoolManager.instance.ReturnToPool(this.gameObject);
+
+        Rigidbody2D rb2d = this.GetComponent<Rigidbody2D>();
+
+        if (rb2d == null)
+        {
+            return;
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+
+    }
+
+    /// <summary>
+    /// Gets an object from the pool manager and puts it at the location provided with
+    /// the specified rotation
+    /// </summary>
+    /// <param name="position"> New position of the returned game object </param>
+    /// <param name="rotation"> New rotation of the returned game object </param>
+    /// <returns></returns>
+    public virtual GameObject GetPooledObject(Vector3 position, Quaternion rotation)
+    {
+        GameObject go = PoolManager.instance.GetPooledObject(this.gameObject);
+        go.transform.position = position;
+        go.transform.rotation = rotation;
+        go.SetActive(true);
+        return go;
+    }
+
+    public virtual void SetParent(Transform parent)
+    {
+        transform.parent = parent;
+    }
+
+}
