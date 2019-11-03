9796774086c970b275ed80f62f167bbf5d25aa13
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2019-11-03 18:16:06 -0500

    Controller Changes Dautery

---
+++ GameOff2019/Assets/Scripts/Gameplay/HUD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] Text hudText;
+    const string HUDTextPrefix = "Jump Height: ";
+    private Action<EventParam> jumpEventListener;
+
+    private void Awake()
+    {
+        // Need to figure out initialization
+        hudText.text = HUDTextPrefix + 0;
+        jumpEventListener = new Action<EventParam>(HandleJumpEvent);
+        EventManager.AddListener(EventNames.JumpUpdateEvent, jumpEventListener);
+    }
+
+
+
+    void HandleJumpEvent(EventParam jumpStrength)
+    {
+        hudText.text = HUDTextPrefix + jumpStrength.intParam;
+    }
+}
