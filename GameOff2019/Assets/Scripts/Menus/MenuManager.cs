ce19cc96e22983e1d8294f2ce671011d457afc45
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2019-11-05 19:48:39 -0500

    Small Changes, Menus, Cleanup

---
+++ GameOff2019/Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manages navigation through the menu system
+/// </summary>
+public static class MenuManager
+{
+    /// <summary>
+    /// PLACEHOLDER: Goes to the menu with the given name
+    /// </summary>
+    /// <param name="name">name of the menu to go to</param>
+    public static void GoToMenu(MenuNamesEnum name)
+    {
+        switch (name)
+        {
+            case MenuNamesEnum.MainMenu:
+
+                // go to MainMenu scene
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case MenuNamesEnum.PauseMenu:
+
+                // instantiate prefab
+                Object.Instantiate(Resources.Load("PauseMenu"));
+                break;
+        }
+    }
+}
+
