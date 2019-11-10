f03d35daed6a69a28a9d4b51c624f330176b3dea
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2019-11-10 15:06:48 -0500

    new tiles and red doors + switches

---
+++ GameOff2019/Assets/Scripts/Gameplay/OnOffBlockTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffBlockTile : IObject
+{
+
+    public GameObject OnSprite;
+    public GameObject OffSprite;
+
+    public override void TurnOn()
+    {
+        OnSprite.SetActive(true);
+        OffSprite.SetActive(false);
+    }
+
+    public override void TurnOff()
+    {
+        OnSprite.SetActive(false);
+        OffSprite.SetActive(true);
+    }
+
+}
