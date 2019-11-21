a0d3b450a1b39f7648305bdc67e523a7159589dd
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2019-11-21 16:40:17 -0500

    AddedAudio

---
+++ GameOff2019/Assets/Scripts/Utility/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string Name;
+
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float Volume;
+
+    [Range(0f, 5f)]
+    public float Pitch;
+
+    public bool Loop;
+    public bool IsBackgroundMusic;
+
+    [HideInInspector]
+    public AudioSource source;
+}
