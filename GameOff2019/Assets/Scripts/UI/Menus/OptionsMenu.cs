8382470baa9d835dc4ab3a5bdbbf4bee8a7e6ca4
Dautery91 <54295241+Dautery91@users.noreply.github.com>
2019-11-27 12:48:38 -0500

    AudioAndMenuUpdates

--- GameOff2019/Assets/Scripts/UI/Menus/OptionsMenu.cs
+++ GameOff2019/Assets/Scripts/UI/Menus/OptionsMenu.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public void SetVolume(float volume)
+    public AudioMixer MainMixer;
+
+    //private AudioMixerGroup musicGroup;
+    //private AudioMixerGroup sfxGroup;
+
+    public void SetMasterVolume(float volume)
     {
+        MainMixer.SetFloat("MasterVolume", volume);
+    }
 
+    public void SetMusicVolume(float volume)
+    {
+        MainMixer.SetFloat("MusicVolume", volume);
+    }
 
-        Debug.Log(volume);
+    public void SetSFXVolume(float volume)
+    {
+        MainMixer.SetFloat("SFXVolume", volume);
     }
 }
