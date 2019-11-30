58d4eb4cb9c13bf92ad609301ef02c2177ca1f08
vjs22334 <jeyasurya.v@example.com>
2019-11-30 20:09:13 +0530

    created a basic scrollable list for level select

---
+++ GameOff2019/Assets/LevelSelectButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    public level level;
+    public Text buttonText;
+
+    public void Initialize(){
+        buttonText.text = level.name;
+    }
+
+    public void levelSelect(){
+        GameManager.instance.LoadSpecificLevel(level.name);
+    }
+   
+}
